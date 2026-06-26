3c57392a84af265795cc8e95f1ee0391be9be76f
Micha Schirmer <michajava8@gmail.com>
2026-06-26 12:26:00 +0200

    csvreader, save_books

--- 03_books_v01/main.py
+++ 03_books_v01/main.py
@@ -2,9 +2,11 @@ from models import Book
 from crud import BookRepository
 from db_connect import connect_db
 from csv_reader import load_csv
+from db_schema import create_new_books_table
 
 def main():
     conn = connect_db()
+    create_new_books_table(conn)
     repo =  BookRepository(conn)
     #repo.save( Book(title="Schönes Kochbuch",author="Maxe", genre="Sachbuch",published_year=2000))
     
---
+++ 03_books_v01/readme.md
@@ -0,0 +1,17 @@
+# Books-DB
+## Version 01
+
+## Feature
+
+* save(Book)
+* save_books(Book-List)
+* read csv
+## Struktur
+
+* models:Book
+* crud:BookRepository
+* service:csv
+* database: connect, schema
+* main: Test Feature
+## Resources
+* books.csv
