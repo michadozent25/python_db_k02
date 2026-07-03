3725b84d32744025ce7e455e5df5b2c56cfe3478
Micha Schirmer <michajava8@gmail.com>
2026-07-03 12:57:05 +0200

    Todo - User - Alchemy: TodoRepository

--- 07_todo_alchemy_v01/crud.py
+++ 07_todo_alchemy_v01/crud.py
@@ -20,6 +20,34 @@ class TodoRepository():
         return todo
     def find_todo_by_user(self,user_id:int)->list[Todo]:
         return self.session.query(Todo).filter(Todo.user_id==user_id).all()
+    
+
+    def update_todo_state(self, todo_id:int, new_state:str)->Todo | None:
+        # TODO check new_state
+        allowed = {"OPEN","IN_PROGRESS","DONE"}
+        if new_state not in allowed:
+            raise ValueError(f"Invalid State, allowed: {allowed}")
+
+
+        todo = self.session.get(Todo,todo_id)
+        if not todo:
+            return None
+        todo.state = new_state
+        self.session.commit()
+        self.session.refresh(todo)
+        return todo
+
+
+
+    def find_todos_by_task(self,user_id:int,task:str)->list[Todo]:
+        return (
+            self.session.query(Todo)
+            .filter(
+                Todo.user_id==user_id, 
+                Todo.task.ilike(f"%{task}%")
+            )
+            .all()
+        )
 
 
     
