68536e7d218f7fb218bb3e05dbedf9683887795b
Micha Schirmer <michajava8@gmail.com>
2026-07-07 08:32:03 +0200

    passwort argon2

--- 08_password/main.py
+++ 08_password/main.py
@@ -10,6 +10,7 @@ print(hashed.decode('utf-8')) # bytes zu String -> in DB speichern
 
 print(bcrypt.checkpw(password.encode('utf-8'),hashed))
 
+bcrypt.hashpw()
 
 from argon2 import PasswordHasher
 from argon2.exceptions import VerifyMismatchError
